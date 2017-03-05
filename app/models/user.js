70be562cca1b089dba59519b73c698d633b4361c
spikers <timfengineering@gmail.com>
2017-03-05 15:02:28 -0800

    Completed preliminary authentication. I get information and populate the fields based on that. Still needs a lot of work. This work is far beyond my scope currently and I need to spend a lot of time fixing this stuff. I'd recommend resetting to a HEAD@ this + 2 or 3. This is also a version with a ton of comments

--- app/models/user.js
+++ app/models/user.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('User', new Schema({
-  username: {type: String, required: true},
-  //password: {type: String, required: true},
+  username: String,
+  /*password: {type: String, required: true},*/
   name: String,
   age: Number,
   email: String,
   gender: String,
   biography: String,
-  admin: Boolean
+  fbToken: {type: String, required: true},
+  admin: Boolean,
+  dates: Array
 }));
