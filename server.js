a780386bdf253a2830c23e9d1d9e51b06e472027
spikers <timfengineering@gmail.com>
2017-03-11 01:49:17 -0800

    Added 2 HTML files: yelp.html and choose.html: These are used to test the Yelp functionality. Currently, making a POST request to the localhost:8000/api/hangout/activity and /api/hangout/restaurant endpoints (url encoded) will return the required information. No DB interaction, simply returns top 3 results for activities. There is a finding algorithm for Events. Need to implement a finding algorithm for restaurants, may never get around to it. Currently does not integrate with the DB. Desperately needs a refactor (~500LOC in one file).

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ var jwt = require('jsonwebtoken');
 var config = require('./config');
 var User = require('./app/models/user');
 
+/*Yelp*/
+var request = require('request');
+var qs = require('querystring');
+
 var fs = require('fs');
+
+var Promise = require('bluebird');
 
 // This asks for the POST information to be application/x-www-urlencoded
 // Instead of using data: {key: value} format.
@@ -18,7 +24,7 @@ var port = process.env.PORT || 8000;
 
 //Database Part************************
 var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+mongoose.Promise = Promise;
 
 mongoose.connect(config.database);
 app.set('secret', config.secret);
@@ -52,38 +58,40 @@ router.use(function(req, res, next) {
   next();
 });
 
+app.use(express.static('public'));
+
 /*
 If you go to 'localhost/' then send the index.html file.
 The reason why this works is I'm using the variable 'router', not 'apiRouter'. 
 'router' is bound below, in my `app.use('/', router);`
 */
-router.get('/', function(req, res) {
-  fs.readFile('./index.html', 'utf8', (err, data) => {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    res.send(data);
-  });
-});
+// router.get('/', function(req, res) {
+//   // fs.readFile('./public/index.html', 'utf8', (err, data) => {
+//   //   if (err) {
+//   //     res.send(err);
+//   //     return;
+//   //   }
+//   //   res.send(data);
+//   // });
+// });
 
-router.get('/login', function(req, res) {
-  fs.readFile('./login.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
-});
+// router.get('/login', function(req, res) {
+//   fs.readFile('./login.html', 'utf8', (err, data) => {
+//     res.send(data);
+//   });
+// });
 
-router.get('/app/css/:css', function(req, res) {
-  fs.readFile('./app/css/' + req.params.css, 'utf8', (err, data) => {
-    res.send(data);
-  });
-});
+// router.get('/app/css/:css', function(req, res) {
+//   fs.readFile('./app/css/' + req.params.css, 'utf8', (err, data) => {
+//     res.send(data);
+//   });
+// });
 
-router.get('/app/js/:js', function(req, res) {
-  fs.readFile('./app/js/' + req.params.js, 'utf8', (err, data) => {
-    res.send(data);
-  });
-});
+// router.get('/app/js/:js', function(req, res) {
+//   fs.readFile('./app/js/' + req.params.js, 'utf8', (err, data) => {
+//     res.send(data);
+//   });
+// });
 
 /*Initialization. But this time, it's for 'localhost/api'*/
 apiRouter.use(function(req, res, next) {
@@ -212,7 +220,268 @@ function handlePut(err, user, req, res) {
 }
 
 
-/*Register Our Routes Here****************/
+
+
+/*********
+Yelp
+*********/
+/************
+Refresh this token every 100 days
+*/
+var yelpToken = config.yelpToken;
+
+/*************
+Remove this for now. We could use it, but it counts towards our API calls. We only get 25k.
+Save the oAuth2.0 token for later. They're valid for 150 days. Every 100 days, get a new one.
+*/
+// var request_yelp = function(set_parameters, callback) {
+//   //Get token first
+//   request({
+//     method: 'POST', 
+//     url:'https://api.yelp.com/oauth2/token', 
+//     headers: {'content-type': 'application/x-www-form-urlencoded'}, 
+//     form:{'client_id': process.env.client_id, 'client_secret': process.env.client_secret, grant_type: 'client_credentials'}
+//   }, cb);
+
+// };
+
+var dateRouter = express.Router();
+var yelpRouter = express.Router();
+
+yelpRouter.use(function(req, res, next) {
+  next();
+});
+
+yelpRouter.route('/')
+  .get(function(req, res) {
+    fs.readFile('./public/yelp.html', 'utf8', (err, data) => {
+      if (err) res.send(err);
+      res.send(data);
+    });
+  });
+
+dateRouter.use(function(req, res, next) {
+  next();
+});
+
+dateRouter.route('/restaurant')
+  .post(function(req, res) {
+
+    //TPADD Planning to use the DB to pull the location
+    req.body.latitude = 33.6506;
+    req.body.longitude = -117.7435; //irvine spectrum
+
+    //If you want a smaller restaurant pool, then choose this
+    req.body.price = '1,2';
+    req.body.limit = 20;
+
+    //This can be vegan and/or vegetarian or anything
+    let categories = ['restaurants'];
+
+
+    let promiseArray = getEvent(req.body, categories);
+
+    //When they all resolve, I'm gonna return all the values. This is an array of yelp objs
+    Promise.all(promiseArray).then(values => {
+      let restaurantListObject = convertValueArrayAndCategoriesToObject(values, categories)
+            .restaurants.businesses;
+      let randomizedRestaurant = randomizeSelection(restaurantListObject)
+
+      res.status(200).json(randomizedRestaurant);
+    }).catch(err => {console.log(err)});
+  })
+
+dateRouter.route('/activity')
+  .post(function(req, res) {
+    console.log('Activity Endpoint Called');
+    req.body.latitude = 33.6506;
+    req.body.longitude = -117.7435; //irvine spectrum
+
+    //If you want less activities, then change this
+    req.body.limit = 3;
+
+    //TPADD Planning on making these categories dynamic based on the week
+    let categories = ['movietheaters', 'bowling', 'museums', 'artclasses'];
+
+    //I get an array of promises from getEvent, one for each category. 
+    let promiseArray = getEvent(req.body, categories);
+
+    //When they all resolve, I'm gonna return all the values. This is an array of yelp objs
+    Promise.all(promiseArray).then(values => {
+      let activityListObject = convertValueArrayAndCategoriesToObject(values, categories);
+      res.status(200).json(activityListObject);
+    }).catch(err => {console.log(err)});
+  })
+
+app.use('/yelp', yelpRouter);
+app.use('/hangout', dateRouter);
+
+/**
+ * This returns an array of promises. This is misnamed. This can be an activity or restaurant. 
+ */
+function getEvent(params, categories) {
+  params['sort_by'] = 'rating';
+  //params.radius = params.radius || 22500; //meters
+
+  var promiseArray = categories.map(item => {
+    //For every category, get Yelp data for that category
+    params.radius = 500; //meters
+    params.categories = item;
+    return getYelpData(params);
+  });
+
+  return promiseArray;
+}
+
+//Please do not call this directly. Call getEvent. 
+function getYelpData(params) {
+  let paramString = qs.stringify(params);
+  return new Promise((resolve, reject) => {
+    recursiveYelpHandler(paramString);
+
+      
+
+
+    /*I'm sorry about this. This is to handle the fact that I can't figure out how to nest promises*/
+    function recursiveYelpHandler(paramStr) {
+      request({
+        method: 'GET',
+        url: 'https://api.yelp.com/v3/businesses/search?' + paramStr,
+        headers: {
+          //Token is global
+          Authorization: 'Bearer ' + yelpToken
+        }
+      }, (err, _, body) => {
+        body = parseJSON(body);
+
+        let paramObject = qs.parse(paramStr);
+
+        if (body.total < paramObject.limit && paramObject.radius < 40000) {
+          //Expand range if not enough
+          //console.log(paramObject.categories, paramObject.radius);
+          paramObject.radius *= 2;
+          paramStr = qs.stringify(paramObject);
+          recursiveYelpHandler(paramStr);;
+        } else if (body.total < paramObject.limit) {
+          //If range is too large, give up
+          reject("Error: Out of Range");
+        } else if (body.total >= paramObject.limit) {
+          //It worked!
+          resolve(body);
+        } else {
+          //???
+          console.log("Cosmic Rays at getEvent");
+        }
+      });
+    }
+
+
+
+
+
+
+
+
+  })
+}
+
+
+
+
+//Always wrap your JSON.parse in a try-catch block. This simply replaces JSON.parse with the safer version
+function parseJSON(item, times = 0) {
+  try {
+    return JSON.parse(item);
+  } catch (e) {
+    if (times < 3) {
+      return parseJSON(item, ++times);
+    } else {
+      console.log(e);
+      return {'error': 'Random String: crnaF27Qgw'};
+    }
+  }
+}
+
+
+//This helper function just makes [restaurant] & [{yelpObj}, {yelpObj}] into 
+/*
+{
+  restaurant: {
+    businesses: [
+      {yelpObj}, 
+      {yelpObj}
+    ]
+  }
+}
+*/
+function convertValueArrayAndCategoriesToObject(valueArray, categories) {
+  let valueObject = {};
+  for (let i = 0; i < categories.length; i++) {
+    valueObject[categories[i]] = valueArray[i];
+  }
+  return valueObject;
+}
+
+function getRestaurant(params, res) {
+  /*************
+  //Use the token to gain access
+  //token = parseJSON(body)['access_token'];
+  */
+  //console.log(process.env.token);
+  /*
+    Epoch timestamp: 1488034800
+    Timestamp in milliseconds: 1488034800
+    Use 1488034800
+    Human time (GMT): Sat, 25 Feb 2017 15:00:00 GMT
+    Human time (your time zone): 2/25/2017, 7:00:00 AM
+  */
+  //term=restaurants&latitude=' + params.lat + '&longitude=' + params.long + '&limit=2&price=1,2&sort_by=rating&radius=500&open_at=1488034800
+  //params.term = 'restaurants';
+  //params.term = 'activities';
+  params.limit = 20;
+  params.price = '1,2';
+  params['sort_by'] = 'rating';
+  params.radius = params.radius || 500; //meters
+  var paramString = qs.stringify(params);
+  //console.log(paramString);
+
+  request({
+    method: 'get',
+    url: 'https://api.yelp.com/v3/businesses/search?' + paramString,
+    headers: {
+      Authorization: 'Bearer ' + yelpToken
+    }
+  }, (err, _, body) => {
+    body = parseJSON(body);
+    //console.log(body.total);
+
+    if (body.total < 3 && params.radius < 25000) {
+      params.radius *= 2;
+      getYelpData(params, res);
+    } else if (body.total < 3) {
+      res.end("Error: Out of Range");
+    } else if (body.total > 3) {
+      randomizeSelection(body, res);
+    }
+  });
+
+  //fs.writeFile('./losangeles.json', body, 'utf-8');
+}
+
+function randomizeSelection(choicesObject) {
+  let index = Math.floor(Math.random() * Object.keys(choicesObject).length);
+  let selection = choicesObject[index];;
+  console.log(selection);
+  return selection;
+}
+
+//getYelpData({ latitude: '33.946232', longitude: '-117.678676', 'open_at': 1488672409 });
+
+
+
+/****************
+Register Our Routes Here
+****************/
 
 app.use('/', router);
 app.use('/api', apiRouter);
