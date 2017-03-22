a0b269daf332b54b00273964812a489e23ecbbec
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-03-22 12:37:50 -0700

    took out comments

--- public/js/facebook_passport.js
+++ public/js/facebook_passport.js
@@ -3,28 +3,22 @@
 import mongoose from 'mongoose';
 var Promise = require('bluebird');
 
-
-
 var FacebookStrategy = require('passport-facebook').Strategy;
 // load up the user model
-var User       = require('./fb_user');
+var User = require('./fb_user');
 
 // load the auth variables
 var configAuth = require('../../config'); // use this one for testing
 
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
+// passport session setup ==================================================
     // required for persistent login sessions
     // passport needs ability to serialize and unserialize users out of session
-
     // used to serialize the user for the session
     passport.serializeUser(function (user, done) {
         done(null, user.id);
     });
-
     // used to deserialize the user
     passport.deserializeUser(function (id, done) {
         User.findById(id, function (err, user) {
@@ -32,74 +26,7 @@ module.exports = function(passport) {
         });
     });
 
- // =========================================================================
- // LOCAL LOGIN =============================================================
- // =========================================================================
- // we are using named strategies since we have one for login and one for signup
- // by default, if there was no name, it would just be called 'local'
-
- // passport.use('local-login', new LocalStrategy({
- // // by default, local strategy uses username and password, we will override with email
- // usernameField : 'email',
- // passwordField : 'password',
- // passReqToCallback : true // allows us to pass back the entire request to the callback
- // },
- // function(req, email, password, done) { // callback with email and password from our form
- //
- // // find a user whose email is the same as the forms email
- // // we are checking to see if the user trying to login already exists
- // User.findOne({ 'local.email' :  email }, function(err, user) {
- // // if there are any errors, return the error before anything else
- // if (err)
- // return done(err);
- //
- // // if no user is found, return the message
- // if (!user)
- // return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
- //
- // // if the user is found but the password is wrong
- // if (!user.validPassword(password))
- // return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
- //
- // // all is well, return successful user
- // return done(null, user);
- // });
- //
- // }));
-
-
-    // =========================================================================
-    // FACEBOOK ================================================================
-    // =========================================================================
-    // User.findOne({ 'local.email' :  email }, function(err, user) {
-    // // if there are any errors, return the error before anything else
-    // if (err)
-    // return done(err);
-    //
-    // // if no user is found, return the message
-    // if (!user)
-    // return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-    //
-    // // if the user is found but the password is wrong
-    // if (!user.validPassword(password))
-    // return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-    //
-    // // all is well, return successful user
-    // return done(null, user);
-    // });
-
-    // User.findOne({'fbToken': req.body.user}, function (err, userObj) {
-    //     userObj.hangouts.unshift(hangout);
-    //     userObj.save(function (err) {
-    //         if (err) {
-    //             res.status(401).json('Error', err);
-    //             return;
-    //         }
-    //     });
-    // });
-
-
-
+// FACEBOOK ================================================================
     var fbStrategy = configAuth.facebookAuth;
     //fbStrategy.passReqToCallback = true;  // allows us to pass in the req from our route (lets us check if a user is logged in or not)
     passport.use(new FacebookStrategy(fbStrategy,
--- public/js/fb_routes.js
+++ public/js/fb_routes.js
@@ -1,32 +1,13 @@
 module.exports = function(app, passport) {
-
-// normal routes ===============================================================
-
-    // // show the home page (will also have our login links)
-    // app.get('/', function (req, res) {
-    //     res.render('index.ejs');
-    // });
-    //
-    // // PROFILE SECTION =========================
-    // app.get('/profile', isLoggedIn, function (req, res) {
-    //     res.render('profile.ejs', {
-    //         user: req.user
-    //     });
-    // });
-
-    // LOGOUT ==============================
+// LOGOUT ==============================
     app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
 
-    // =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
-
     // send to facebook to do the authentication
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
-
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
@@ -34,10 +15,7 @@ module.exports = function(app, passport) {
             failureRedirect: '/'
         }));
 
-// =============================================================================
 // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
-// =============================================================================
-
     // send to facebook to do the authentication
     app.get('/connect/facebook', passport.authorize('facebook', {scope: 'email'}));
 
@@ -48,9 +26,7 @@ module.exports = function(app, passport) {
             failureRedirect: '/'
         }));
 
-// =====================================
 // LOGOUT ==============================
-// =====================================
 app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
@@ -59,12 +35,9 @@ app.get('/logout', function(req, res) {
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
-
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
-
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
-
